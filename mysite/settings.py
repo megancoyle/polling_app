136d06afe8d120b2744ff6d9f7a49254d92e0388
Megan Coyle <meg@mcoyle.com>
2017-10-15 20:36:46 -0400

    Update installed apps in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
